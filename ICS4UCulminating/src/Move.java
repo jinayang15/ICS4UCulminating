1f9625879f5975d0e6ab3514849cc280af602884
jinayang15 <jinayang15@gmail.com>
2023-05-30 09:40:10 -0400

    bleh

---
+++ ICS4UCulminating/src/Move.java
@@ -0,0 +1,9 @@
+// Class Move 
+// All Pokemon Moves will inherit this class
+public class Move {
+	private Type type;
+	private String category;
+	private int atkPower;
+	private int accuracy;
+	private int pp;
+}
