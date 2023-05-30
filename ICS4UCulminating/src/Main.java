7de1bd07f2879c6810b7ce79c257cc42dc05bce7
raygu <raygu@10.241.231.1>
2023-05-30 18:01:15 -0400

    Changed the file name to TypeEffectiveness

--- ICS4UCulminating/src/Main.java
+++ ICS4UCulminating/src/Main.java
@@ -2,7 +2,7 @@ import java.io.*;
 public class Main {
 
 	public static void main(String[] args) throws IOException{
-		System.out.println(Type.getTypeEffectiveness(0, 0));
+		System.out.println(TypeEffectiveness.getTypeEffectiveness(4, 6));
 	}
 
 }
