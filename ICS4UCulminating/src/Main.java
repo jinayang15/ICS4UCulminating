2a40367f01123a58f7c9f831a6cd4bb0b54988e9
jinayang15 <jinayang15@gmail.com>
2023-06-01 22:48:28 -0400

    changed type effectiveness class to just the type class

--- ICS4UCulminating/src/Main.java
+++ ICS4UCulminating/src/Main.java
@@ -2,7 +2,7 @@ import java.io.*;
 public class Main {
 	// We may need double typing rip
 	public static void main(String[] args) throws IOException{
-		System.out.println(TypeEffectiveness.getTypeEffectiveness(4, 6));
+		System.out.println(Type.getTypeEffectiveness(4, 6));
 	}
 
 }
