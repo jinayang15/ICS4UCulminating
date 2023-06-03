3d5f82b171bd6f7923d40c64eb6ef93c0ef750f4
jinayang15 <jinayang15@gmail.com>
2023-06-03 16:12:37 -0400

    updates all moves txt

--- ICS4UCulminating/src/Main.java
+++ ICS4UCulminating/src/Main.java
@@ -4,7 +4,6 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		System.out.println(PokeType.getTypeEffectiveness(4, 6));
 		GameFunctions.importPokemon();
-		GameFunctions.importMoves();
 	}
 
 }
