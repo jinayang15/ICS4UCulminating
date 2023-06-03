505c6377443a8b7003c127952aee07ade524f9e3
Ray <Ray@10.0.0.207>
2023-06-03 11:58:27 -0400

    created GameFunctions class

---
+++ ICS4UCulminating/src/GameFunctions.java
@@ -0,0 +1,8 @@
+// The GameFunctions class is used to perform all the game functions 
+// These game functions include text file reading, etc. 
+public class GameFunctions {
+	
+	public GameFunctions () {
+		
+	}
+}
